418d1fa2b7be0ae90f8bbe5f3f5b4c19e57fbdfa
Boaz Webber <boaz@boazwebber.com>
2025-03-10 15:01:25 -0400

    Surrounded Scanner with 'try' block

--- IPAddressToBinary.java
+++ IPAddressToBinary.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 
 public class IPAddressToBinary {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the IP address:");
-        String ip = scan.nextLine();
-        String[] splitIP = ip.split("\\.");
-        StringBuilder binaryIP = new StringBuilder();
-        for (String part : splitIP) {
-            String binaryPart = Integer.toBinaryString(Integer.parseInt(part));
-            while (binaryPart.length() < 8) {
-                binaryPart = "0" + binaryPart;
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter the IP address:");
+            String ip = scan.nextLine();
+            String[] splitIP = ip.split("\\.");
+            StringBuilder binaryIP = new StringBuilder();
+            for (String part : splitIP) {
+                String binaryPart = Integer.toBinaryString(Integer.parseInt(part));
+                while (binaryPart.length() < 8) {
+                    binaryPart = "0" + binaryPart;
+                }
+                binaryIP.append(binaryPart + ".");
             }
-            binaryIP.append(binaryPart + ".");
+            System.out.println("Binary equivalent of " + ip + " is " + binaryIP);
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        System.out.println("Binary equivalent of " + ip + " is " + binaryIP);
     }
 }
--- NetworkAddressIdentication.java
+++ NetworkAddressIdentication.java
@@ -2,19 +2,23 @@ import java.util.Scanner;
 
 public class NetworkAddressIdentication {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the IP address:");
-        String ip = scan.nextLine();
-        System.out.println("Enter the subnet mask:");
-        String subnet = scan.nextLine();
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Enter the IP address:");
+            String ip = scan.nextLine();
+            System.out.println("Enter the subnet mask:");
+            String subnet = scan.nextLine();
 
-        String[] splitIP = ip.split("\\.");
-        String[] splitSubnet = subnet.split("\\.");
-        String[] networkParts = new String[4];
-        for (int i = 0; i < 4; i++) {
-            networkParts[i] = String.valueOf(Integer.parseInt(splitIP[i]) & Integer.parseInt(splitSubnet[i]));
+            String[] splitIP = ip.split("\\.");
+            String[] splitSubnet = subnet.split("\\.");
+            String[] networkParts = new String[4];
+            for (int i = 0; i < 4; i++) {
+                networkParts[i] = String.valueOf(Integer.parseInt(splitIP[i]) & Integer.parseInt(splitSubnet[i]));
+            }
+            String networkAddress = String.join(".", networkParts);
+            System.out.println("Network Address: " + networkAddress);
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-        String networkAddress = String.join(".", networkParts);
-        System.out.println("Network Address: " + networkAddress);
     }
 }
